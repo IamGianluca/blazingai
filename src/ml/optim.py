97aa665bc2f0854f6d377bc7ee6b661b44cb83aa
Gianluca Rossi <gr.gianlucarossi@gmail.com>
2022-09-21 19:04:47 -0400

    BUG: fix several bugs due to upstream dependencies being updated (#6)

--- src/ml/optim.py
+++ src/ml/optim.py
@@ -1,15 +1,18 @@
-from typing import Iterable, Optional
-
-import torch
 from torch import optim
-from torch.optim import lr_scheduler
-from torch.optim._multi_tensor import SGD
-from transformers.optimization import AdamW
+from torch.optim import Adam
+from torch.optim.lr_scheduler import (
+    CosineAnnealingWarmRestarts,
+    OneCycleLR,
+    ReduceLROnPlateau,
+)
+from transformers.optimization import AdamW, get_cosine_schedule_with_warmup
+
+# torch.optim.lr_scheduler.OneCycleLR
 
 
 def optimizer_factory(params, hparams):
     if hparams.opt == "adam":
-        return optim.Adam(
+        return Adam(
             params,
             lr=hparams.lr,
             weight_decay=hparams.wd,
@@ -23,13 +26,13 @@ def optimizer_factory(params, hparams):
             weight_decay=hparams.wd,
             correct_bias=True,
         )
-    if hparams.opt == "sam":
-        return SAM(
-            params,
-            lr=hparams.lr,
-            momentum=hparams.mom,
-            weight_decay=hparams.wd,
-        )
+    # if hparams.opt == "sam":
+    #     return SAM(
+    #         params,
+    #         lr=hparams.lr,
+    #         momentum=hparams.mom,
+    #         weight_decay=hparams.wd,
+    #     )
     else:
         raise ValueError("Optimizer not supported yet.")
 
@@ -37,7 +40,7 @@ def optimizer_factory(params, hparams):
 def lr_scheduler_factory(optimizer, hparams, data_loader):
     steps_per_epoch = len(data_loader)
     if hparams.sched == "plateau":
-        return lr_scheduler.ReduceLROnPlateau(
+        return ReduceLROnPlateau(
             optimizer,
             mode="max",
             patience=2,
@@ -46,7 +49,7 @@ def lr_scheduler_factory(optimizer, hparams, data_loader):
             verbose=True,
         )
     elif hparams.sched == "onecycle":
-        return lr_scheduler.OneCycleLR(
+        return optim.lr_scheduler.OneCycleLR(
             optimizer=optimizer,
             max_lr=hparams.lr,
             cycle_momentum=True,
@@ -57,94 +60,16 @@ def lr_scheduler_factory(optimizer, hparams, data_loader):
             epochs=hparams.epochs,
         )
     elif hparams.sched == "cosine":
-        return transformers.get_cosine_schedule_with_warmup(
+        return get_cosine_schedule_with_warmup(
             optimizer=optimizer,
             num_warmup_steps=steps_per_epoch * hparams.warmup_epochs,
             num_training_steps=steps_per_epoch * hparams.epochs,
         )
     elif hparams.sched == "cosine_with_restart":
-        return lr_scheduler.CosineAnnealingWarmRestarts(
+        return CosineAnnealingWarmRestarts(
             optimizer=optimizer,
             T_0=20,
             eta_min=1e-4,
         )
     else:
         raise ValueError("Learning rate scheduler not supported yet.")
-
-
-# copied from https://github.com/moskomule/sam.pytorch/blob/main/sam.py
-class SAM(SGD):
-    """SGD wrapped with Sharp-Aware Minimization
-    Args:
-        params: tensors to be optimized
-        lr: learning rate
-        momentum: momentum factor
-        dampening: damping factor
-        weight_decay: weight decay factor
-        nesterov: enables Nesterov momentum
-        rho: neighborhood size
-    """
-
-    def __init__(
-        self,
-        params: Iterable[torch.Tensor],
-        lr: float,
-        momentum: float = 0,
-        dampening: float = 0,
-        weight_decay: float = 0,
-        nesterov: bool = False,
-        rho: float = 0.05,
-    ):
-        if rho <= 0:
-            raise ValueError(f"Invalid neighborhood size: {rho}")
-        super().__init__(
-            params, lr, momentum, dampening, weight_decay, nesterov
-        )
-        # todo: generalize this
-        if len(self.param_groups) > 1:
-            raise ValueError("Not supported")
-        self.param_groups[0]["rho"] = rho
-
-    @torch.no_grad()
-    def step(self, closure=None) -> torch.Tensor:
-        """
-        Args:
-            closure: A closure that reevaluates the model and returns the loss.
-        Returns: the loss value evaluated on the original point
-        """
-        loss: Optional[torch.Tensor] = None
-        if closure is not None:
-            closure = torch.enable_grad()(closure)
-            loss = closure().detach()
-
-        for group in self.param_groups:
-            grads = []
-            params_with_grads = []
-
-            rho = group["rho"]
-            # update internal_optim's learning rate
-
-            for p in group["params"]:
-                if p.grad is not None:
-                    # without clone().detach(), p.grad will be zeroed by closure()
-                    grads.append(p.grad.clone().detach())
-                    params_with_grads.append(p)
-            device = grads[0].device
-
-            # compute \hat{\epsilon}=\rho/\norm{g}\|g\|
-            grad_norm = torch.stack(
-                [g.detach().norm(2).to(device) for g in grads]
-            ).norm(2)
-            epsilon = grads  # alias for readability
-            torch._foreach_mul_(epsilon, rho / grad_norm)
-
-            # virtual step toward \epsilon
-            torch._foreach_add_(params_with_grads, epsilon)
-            # compute g=\nabla_w L_B(w)|_{w+\hat{\epsilon}}
-            if closure is not None:
-                closure()
-            # virtual step back to the original point
-            torch._foreach_sub_(params_with_grads, epsilon)
-
-        super().step()
-        return loss
--- src/setup.py
+++ src/setup.py
@@ -1,6 +1,5 @@
-from setuptools import setup
-
 import ml
+from setuptools import setup
 
 setup(
     name="ml",
