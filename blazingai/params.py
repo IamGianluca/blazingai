820d66fd1a55aa984772fd4540e6cf3845734aef
Gianluca Rossi <gr.gianlucarossi@gmail.com>
2022-11-24 12:25:10 -0500

    MAINT: add type hints to load_hparams_from_yaml()

--- blazingai/params.py
+++ blazingai/params.py
@@ -1,8 +1,11 @@
+from pathlib import Path
+
+from omegaconf import OmegaConf
 from omegaconf.errors import ConfigAttributeError
 from pytorch_lightning.core.saving import load_hparams_from_yaml
 
 
-def load_cfg(fpath, cfg_name):
+def load_cfg(fpath: Path, cfg_name: str) -> OmegaConf:
     cfg = load_hparams_from_yaml(config_yaml=fpath, use_omegaconf=True)
     cfg = cfg.get(cfg_name)
     try:
