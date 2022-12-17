7a0daea01f6c2d5347decc4f3e1909dbe6c1ea2d
Gianluca Rossi <gr.gianlucarossi@gmail.com>
2022-12-17 10:59:03 -0500

    MAINT: avoid unnecessary try/except block and use hasattr instead

--- blazingai/params.py
+++ blazingai/params.py
@@ -2,14 +2,11 @@ from pathlib import Path
 
 from lightning.pytorch.core.saving import load_hparams_from_yaml
 from omegaconf import DictConfig
-from omegaconf.errors import ConfigAttributeError
 
 
 def load_cfg(fpath: Path, cfg_name: str) -> DictConfig:
-    cfg: DictConfig = load_hparams_from_yaml(config_yaml=fpath, use_omegaconf=True)
+    cfg = load_hparams_from_yaml(config_yaml=fpath, use_omegaconf=True)
     cfg = cfg.get(cfg_name)
-    try:
-        cfg.lr = float(cfg.lr)
-    except ConfigAttributeError:
-        pass
+    if hasattr(cfg, "lr"):
+        cfg.lr = float(cfg.lr)  # type: ignore
     return cfg
