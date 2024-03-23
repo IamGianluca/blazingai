529d97b556bb7abccafd544ef2d01ef87f048b4c
Gianluca Rossi <gr.gianlucarossi@gmail.com>
2024-03-23 17:04:48 -0400

    fix: raise explicit error if config is not found

--- blazingai/params.py
+++ blazingai/params.py
@@ -5,8 +5,12 @@ from omegaconf import DictConfig
 
 
 def load_cfg(fpath: Path, cfg_name: str) -> DictConfig:
-    cfg = load_hparams_from_yaml(config_yaml=fpath, use_omegaconf=True)
-    cfg = cfg.get(cfg_name)
-    if hasattr(cfg, "lr"):
+    all_configs = load_hparams_from_yaml(config_yaml=fpath, use_omegaconf=True)
+    cfg = all_configs.get(cfg_name)
+    if cfg is None:
+        raise ValueError(f"Could not find config {cfg_name} in {fpath}")
+
+    if hasattr(cfg, "lr"):  # convert exponential notation to float
         cfg.lr = float(cfg.lr)  # type: ignore
+
     return cfg
